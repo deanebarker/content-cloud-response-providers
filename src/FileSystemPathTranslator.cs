0b6e50eb34c7ed76d6a29b250d7ef8a9e1e29823
Deane Barker <deane@deanebarker.net>
2022-08-18 13:52:23 +0000

    Removed unused code

--- src/FileSystemPathTranslator.cs
+++ src/FileSystemPathTranslator.cs
@@ -1,24 +1,12 @@
 ﻿using DeaneBarker.Optimizely.ResponseProviders.Models;
 using DeaneBarker.Optimizely.ResponseProviders.Services;
-using EPiServer.Core;
-using EPiServer.ServiceLocation;
-using EPiServer.Web.Routing;
 
 namespace DeaneBarker.Optimizely.ResponseProviders
 {
     public class FileSystemPathTranslator : SimplePathTranslator, IResponseProviderPathTranslator
     {
-        // Public so you can change them if you like
+        // Public so you can change it if you like
         public new string DefaultDocument { get; set; } = "index.html";
-        public new string NotFoundDocument { get; set; } = "404.html";
-
-
-        private IUrlResolver _urlResolver;
-
-        public FileSystemPathTranslator()
-        {
-            _urlResolver = ServiceLocator.Current.GetInstance<UrlResolver>();
-        }
 
         public new string GetTranslatedPath(BaseResponseProvider siteRoot, string requestedPath)
         {
